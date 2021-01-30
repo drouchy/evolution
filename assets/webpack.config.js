e484df79a455710544c5f360320419b696bde47d
David Rouchy <david@rouchy.dev>
2021-01-30 09:29:02 +0000

    replace css framework with tailwindcss

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -31,18 +31,14 @@ module.exports = (env, options) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader'
-          }
+            loader: "babel-loader",
+          },
         },
         {
           test: /\.[s]?css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            'sass-loader',
-          ],
-        }
-      ]
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
+      ],
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
